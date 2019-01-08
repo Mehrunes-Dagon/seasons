b96c69fd29422ff83f144de304a789df5609828c
William <wtpwinberg@gmail.com>
2019-01-07 19:44:12 -0700

    Init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+   // state = {}
+   constructor(props) {
+      // one time setup
+      // data loading not recomended
+      super(props);
+      this.state = {
+         latitude: null,
+         errorMessage: ''
+      };
+      window.navigator.geolocation.getCurrentPosition(
+         position => this.setState({ latitude: position.coords.latitude }),
+         err => this.setState({ errorMessage: err.message }),
+      );
+   }
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+   componentDidMount() {
+      // do data loading here
+      console.log('My component was rendered to the screen.');
+   }
+
+   componentDidUpdate() {
+      // data loading done when component is updated
+      // render called before update.
+      console.log('My component was updated.');
+   }
+
+   componentWillUnmount() {
+      console.log('My component is no longer rendered.');
+   }
+
+   render() {
+      // return JSX
+      const { latitude, errorMessage } = this.state;
+
+      if (errorMessage && !latitude) {
+         return <div>Error: {this.state.errorMessage}</div>;
+      }
+      if (!errorMessage && latitude) {
+         return <div>Latitude: {this.state.latitude}</div>;
+      }
+
+      return <div>Loading...</div>;
+   }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
